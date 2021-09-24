1eed8fbc253859d313937f5d4fb1dd791877f813
jsegobia0 <jsegobia0@yahoo.com>
2021-09-24 14:49:24 -0700

    updated file

--- app.js
+++ app.js
@@ -45,19 +45,53 @@ const teamMenbers = [];
     // 
 // }
 function createManager() {
-    let manager = new Manager();
-    {
-    typ:"input"
-    name:"manager"
-    message: "What is the Manager's name?"
-    }
+    const manager = new Manager();
+    const questions = {
+        type: "input",
+        name: "manager",
+        message: "What is the name of your manager?"
+    };
+
+
+
+    // test("Can set office number via constructor argument", () => {
+    //     const testValue = 100;
+    //     const e = new Manager("Foo", 1, "test@test.com", testValue);
+    //     expect(e.officeNumber).toBe(testValue);
+    //   });
+      
+    //   test('getRole() should return "Manager"', () => {
+    //     const testValue = "Manager";
+    //     const e = new Manager("Foo", 1, "test@test.com", 100);
+    //     expect(e.getRole()).toBe(testValue);
+    //   });
+      
+    //   test("Can get office number via getOffice()", () => {
+    //     const testValue = 100;
+    //     const e = new Manager("Foo", 1, "test@test.com", testValue);
+    //     expect(e.getOfficeNumber()).toBe(testValue);
+    //   });
+    createManager();
 }
+
 // .then(answers => {
     // const manager = new Manager (answers.managerName,  ...)
     // teamMembers.push(manager)
     // createTeam();
 // }
 
+function createEngineer() {
+    const engineer = new Engineer();
+
+}
+
+function createIntern() {
+
+}
+
+function createEmployee() {
+
+}
 // type list of options to call for team members
 
 // build team () =>
